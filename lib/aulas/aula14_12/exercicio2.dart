35c6a390cff7d8fe8434028eedb12a8df5ac171f
Renan S P Vargas <renanspvargas@outlook.com>
2021-12-21 12:44:59 -0300

    completed - list item expands on tap

--- lib/aulas/aula14_12/exercicio2.dart
+++ lib/aulas/aula14_12/exercicio2.dart
@@ -22,83 +22,55 @@ class ListGenerator extends StatefulWidget {
 class _ListGeneratorState extends State<ListGenerator> {
   final _animationTimer = Duration(milliseconds: 500);
   final _items = List.generate(100, (i) => ListItemManager());
+  final controller = ListItemManager();
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      itemCount: _items.length,
       itemBuilder: (context, index) {
         return Card(
           color: Colors.purple,
           child: GestureDetector(
             onTap: () {
               setState(() {
-                _items[index].iconOrientation += 1.0 / 2.0;
                 _items[index].isExpanded = !_items[index].isExpanded;
+                _items[index].iconOrientation += 1.0 / 2.0;
               });
             },
-            child: ClipRect(
-              child: ListTile(
-                trailing: AnimatedRotation(
-                  duration: _animationTimer,
-                  turns: _items[index].iconOrientation,
-                  child: Icon(Icons.arrow_downward),
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text("Item $index"),
+                  trailing: AnimatedRotation(
+                    duration: _animationTimer,
+                    turns: _items[index].iconOrientation,
+                    child: Icon(
+                      Icons.expand_more,
+                    ),
+                  ),
                 ),
-                title: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        "Item $index",
-                        style: TextStyle(
-                            color: Colors.black, backgroundColor: Colors.white),
+                ClipRect(
+                  child: AnimatedAlign(
+                    duration: _animationTimer,
+                    alignment: Alignment.center,
+                    heightFactor: _items[index].isExpanded ? 1 : 0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: const [
+                          FlutterLogo(size: 100.0),
+                          Text(
+                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                          ),
+                        ],
                       ),
                     ),
-                    AnimatedAlign(
-                        alignment: Alignment.center,
-                        duration: _animationTimer,
-                        heightFactor: _items[index].isExpanded ? 1 : 0,
-                        child: Text(
-                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
-                          style: TextStyle(color: Colors.black),
-                        )),
-                    SizedBox(
-                      height: 10,
-                    )
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
-          // child: AnimatedAlign(
-          //   onEnd: () => Container(),
-          //   alignment: Alignment.topLeft,
-          //   duration: _animationTimer,
-          //   heightFactor: _items[index].isExpanded ? 1.5 : 1,
-          //   child: Column(
-          //     children: [
-          //       ListTile(
-          //         tileColor: Colors.deepPurpleAccent,
-          //         title: Text("Item $index"),
-          //         trailing: AnimatedRotation(
-          //           duration: _animationTimer,
-          //           turns: _items[index].iconOrientation,
-          //           child: Icon(Icons.arrow_downward),
-          //         ),
-          //         onTap: () {
-          //           setState(() {
-          //             _items[index].iconOrientation += 1.0 / 2.0;
-          //             _items[index].isExpanded = !_items[index].isExpanded;
-          //           });
-          //         },
-          //       ),
-          //       Container(
-          //         height: 10,
-          //         width: 10,
-          //         color: Colors.cyan,
-          //       ),
-          //     ],
-          //   ),
-          // ),
         );
       },
     );
